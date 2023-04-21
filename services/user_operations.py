f204e59bc45f4aa470a21f3f9d9400a93517bc39
BaoHongJiangGames <baobaoandworld@gmail.com>
2023-04-22 04:07:40 +0800

    解决很傻的方法命名冲突

--- services/user_operations.py
+++ services/user_operations.py
@@ -1,18 +1,16 @@
 from sqlalchemy.orm import Session
-from app.db.crud import get_user_by_id, get_user_by_username, update_balance_in_database, create_user
+from app.db.crud import get_user_by_id as crud_get_user_by_id, get_user_by_username as crud_get_user_by_username, update_balance_in_database, create_user as crud_create_user
 from app.models.user import User
 from app.schemas.user import UserCreate
 
 def get_user_by_id(db: Session, user_id: int):
-		return get_user_by_id(db, user_id)
+    return crud_get_user_by_id(db, user_id)
 
 def get_user_by_username(db: Session, username: str):
-    return get_user_by_username(db, username)
-
-
+    return crud_get_user_by_username(db, username)
 
 def create_user(db: Session, user: UserCreate):
-  return create_user(db, user)
+    return crud_create_user(db, user)
 
 def update_user_balance(db: Session, user_id: int, cost: float):
     # Retrieve user data from the database using user_id
@@ -22,5 +20,3 @@ def update_user_balance(db: Session, user_id: int, cost: float):
 
         # Update user's balance in the database
         update_balance_in_database(db, user_id, new_balance)
-
-
