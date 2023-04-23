58f55d2443e04684d5c68f9e5844920174d4c795
BaoHongJiangGames <baobaoandworld@gmail.com>
2023-04-23 23:13:53 +0800

    修正账户名

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 class UserCreate(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=3, max_length=30, regex="^[a-zA-Z0-9_]+$")
+    password: str = Field(..., min_length=8, max_length=128, regex="^[ -~]+$")
